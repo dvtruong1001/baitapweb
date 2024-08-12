3ce05fcc5ee8eec27528ed3ff05b8a6e143031a3
Đặng Văn Trưởng <dangvanatruong0501@gmail.com>
2024-08-12 16:43:58 +0700

    Xong baif 1

--- index.php
+++ index.php
@@ -23,13 +23,7 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="script/script.js"></script>
-    <script>
-        $(document).ready(function() {
-
-
-
-        })
-    </script>
+    
 </body>
 
 </html>
